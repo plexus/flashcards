15e92d2339e6fbd704e1d2696af6ae8d7ec275e8
Lukas Rieder <l.rieder@gmail.com>
2015-09-16 23:06:16 +0200

    Step 1: getting started
    * Simple command line interface, use like ruby flashcards.rb

---
+++ README.md
@@ -0,0 +1,11 @@
+# flashcards
+
+A simple app that allows you to memorise with the flash cards method.
+Currently the data is a fixed set of Japanese hiragana, saved in `hiragana.csv`.
+
+## Usage
+
+Simply start on the command line:
+
+    $ ruby flashcards.rb
+
---
+++ flashcards.rb
@@ -0,0 +1,16 @@
+require "csv"
+require "io/console"
+
+cards = CSV.read("hiragana.csv", headers: true).to_a
+
+puts "Welcome to flashcards. Press any™ key to continue, Exit using CTRL+C\n"
+pile = []
+loop do
+  pile = cards.shuffle if pile.empty?
+  romaji, hiragana = pile.pop
+  print "\r\n #{hiragana}"
+  exit if STDIN.getch == "\u0003"
+  puts " #{romaji}"
+  system("say #{romaji} &") if ENV["SPEAK"]
+end
+
