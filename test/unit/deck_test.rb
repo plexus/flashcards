5d310c6cfb659f20b1862e226f2c5a642d6f189d
Lukas Rieder <l.rieder@gmail.com>
2015-09-22 14:49:36 +0200

    Added tests for Deck::Card#==

--- test/unit/deck_test.rb
+++ test/unit/deck_test.rb
@@ -2,6 +2,31 @@ require_relative "../test_helper.rb"
 
 describe Deck do
 
+  describe Deck::Card do
+
+    describe "#==" do
+      it "should compare against id" do
+        assert Deck::Card.new(0, "a", "A") == Deck::Card.new(0, "a", "A")
+        assert Deck::Card.new(1, "a", "A") != Deck::Card.new(0, "a", "A")
+      end
+
+      it "should compare against front" do
+        assert Deck::Card.new(0, "a", "A") == Deck::Card.new(0, "a", "A")
+        assert Deck::Card.new(0, "b", "A") != Deck::Card.new(0, "a", "A")
+      end
+
+      it "should compare against back" do
+        assert Deck::Card.new(0, "a", "A") == Deck::Card.new(0, "a", "A")
+        assert Deck::Card.new(0, "a", "B") != Deck::Card.new(0, "a", "A")
+      end
+
+      it "should compare wether the give object is a Deck::Card" do
+        assert Deck::Card.new(0, "a", "A") == Deck::Card.new(0, "a", "A")
+        assert Deck::Card.new(0, "a", "A") != stub(id: 0, front: "a", back: "A")
+      end
+    end
+
+  end
 
   let(:deck) { Deck.read_from_csv("hiragana") }
 
